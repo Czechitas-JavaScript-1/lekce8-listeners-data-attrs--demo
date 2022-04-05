f20c87c23dd85433bd5dd4a0f2580fee5aa3cdbc
karolyim <matej.karolyi@gmail.com>
2022-04-05 19:09:15 +0200

    Add event listeners to the buttons

---
+++ pozdravy.js
@@ -0,0 +1,7 @@
+const tlacitka = document.querySelectorAll('.tlacitko-pozdravu');
+
+tlacitka.forEach((tlacitko) => {
+    tlacitko.addEventListener('click', (udalost) => {
+        console.log('Ahoj ' + udalost.target.innerHTML);
+    });
+});
